fe95ff27aa165b4529e718543aebad0b9e5ebcc0
Eunji Kim <ekim1396@gmail.com>
2021-12-23 19:11:36 -0500

    updated

--- tests/test_webtoon_data.py
+++ tests/test_webtoon_data.py
@@ -1,4 +1,9 @@
 from webtoon_data import webtoon_data
+import pytest
+import pandas as pd
+import requests
+import re
+import json
 
 def test_get_webtoon_genre_list():
     assert get_webtoon_genre_list() == ['Drama', 'Fantasy', 'Comedy', 'Action', 'Slice of life', 'Romance', 'Superhero', 'Sci-fi', 'Thriller', 'Supernatural', 'Mystery', 'Sports', 'Historical', 'Heart-warming', 'Horror', 'Informative']
